3c0e4e9a53ea8801a970a64f783dadd44b99c2d2
Saahil Chaddha <160884291+schaddha05@users.noreply.github.com>
2026-07-05 15:36:53 -0500

    Add: minimum-window-with-characters - submission-22

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-22.py
@@ -0,0 +1,40 @@
+class Solution:
+    from collections import defaultdict
+    import math
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        
+        tFreq = defaultdict(int)
+        for c in t:
+            tFreq[c] += 1
+        
+        window = defaultdict(int)
+        l = 0 
+        have = 0
+        start = 0 
+        end = 0 
+        length = math.inf
+        for r in range(len(s)):
+            if s[r] in tFreq:
+                window[s[r]] += 1
+            
+            if s[r] in tFreq and window[s[r]] == tFreq[s[r]]:
+                have += 1
+            while have == len(tFreq):
+                if r - l + 1 < length:
+                    length = r - l + 1
+                    start = l
+                    end = r
+
+                window[s[l]] -= 1
+                if s[l] in tFreq and window[s[l]] < tFreq[s[l]]:
+                    have -= 1
+              
+                l += 1
+
+
+        return "" if length == math.inf else s[start:end+1]
+
+
+
