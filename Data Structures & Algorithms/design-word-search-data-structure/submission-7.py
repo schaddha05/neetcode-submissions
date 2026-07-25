3b64611f4d03a556582b9ad6499ee1fae25d6833
Saahil Chaddha <160884291+schaddha05@users.noreply.github.com>
2026-07-24 22:23:57 -0400

    Add: design-word-search-data-structure - submission-7

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-7.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False 
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TreeNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TreeNode()
+            curr = curr.children[c]
+        
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(index, root):
+            curr = root 
+            for i in range(index, len(word)):
+                j = i
+                if word[i] == '.':
+                    for child in curr.children.values():
+                        if dfs(j + 1, child):
+                            return True
+                    return False
+                elif word[i] not in curr.children:
+                    return False
+                else:
+                    curr = curr.children[word[i]]
+
+            return curr.word
+        
+        return dfs(0, self.root)
