df7e2ead6f79702a17ac3325982edc8a93cc941f
Saahil Chaddha <160884291+schaddha05@users.noreply.github.com>
2026-07-26 16:31:07 -0400

    Add: subsets - submission-13

---
+++ Data Structures & Algorithms/subsets/submission-13.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(i, path):
+            if i == len(nums):
+                res.append(list(path))
+                return 
+            
+            # either add current number or skip
+            path.append(nums[i]) 
+            dfs(i+1, path)
+
+            path.pop()
+            dfs(i+1, path)
+
+
+
+        dfs(0, [])
+        return res
+            
+
