946471c326e8a7d8502926e48ba1a988fadea791
Saahil Chaddha <160884291+schaddha05@users.noreply.github.com>
2026-07-28 22:02:36 -0400

    Add: generate-parentheses - submission-10

---
+++ Data Structures & Algorithms/generate-parentheses/submission-10.py
@@ -0,0 +1,39 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        openCount = 0
+        closedCount = 0
+
+        def dfs(path):
+            nonlocal openCount
+            nonlocal closedCount
+
+            if len(path) == 2 * n:
+                res.append(''.join(path))
+                return
+            
+            if closedCount > openCount: # invalid path, so prune
+                return 
+            
+            # add "("
+            if openCount < n:
+                path.append('(')
+                openCount += 1
+                dfs(path)
+                path.pop()
+                openCount -= 1
+
+            # add ")"
+            if closedCount < n:
+                path.append(')')
+                closedCount += 1
+                dfs(path)
+                path.pop()
+                closedCount -= 1
+            
+           
+        
+        dfs([])
+        return res
+
+            
